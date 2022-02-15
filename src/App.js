c3fd5354d22352657a9c69814ef0481d962c07b9
DILEESHA1 <dileeshanawarathna18@gmail.com>
2022-02-15 17:17:27 +0530

    c.v.1

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./app.css";
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Dndex from "./components/earth/Dndex";
+
+
+const Container = styled.div`
+  height: 100vh;
+  margin:0;
+  padding:0;
+  width: 100%;
+  background-color:#01040c;
+`;
+
+function App() {
+  return (
+    <Container>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Dndex/>
+        </Suspense>
+      </Canvas>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/earth/Dndex.js
@@ -0,0 +1,96 @@
+import {useEffect, useRef} from "react";
+import "./dnd.css";
+import { OrbitControls ,Stars,RoundedBox} from "@react-three/drei";
+import * as THREE from "three";
+
+import EarthDayMap from "../../assests/textures/earthdaymap.jpg";
+// import Nigths from '../../assests/textures/nightmap.jfif'
+import Normal from "../../assests/textures/normalmap.jfif";
+import Clouds from "../../assests/textures/cloudsmap.jfif";
+import Specular from "../../assests/textures/specular.jfif";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { SphereGeometry, TextureLoader } from "three";
+
+const Dndex = () => {
+  const [daymap, normalmap, cloudsmap, specularmap] = useLoader(TextureLoader, [
+    EarthDayMap,
+    Normal,
+    Clouds,
+    Specular,
+  ]);
+
+  const earthRef = useRef();
+  const cloudyRef = useRef();
+
+  useFrame(({clock})=>{
+const time = clock.getElapsedTime();
+
+earthRef.current.rotation.y = time / 7;
+
+cloudyRef.current.rotation.y = time / 7
+  })
+
+
+  return (
+    <>
+    <pointLight color="#f6f6fb" position={[2,0,5]} intensity = {1.2}/>
+ <Stars
+  radius={300} // Radius of the inner sphere (default=100)
+  depth={50} // Depth of area where stars should fit (default=50)
+  count={10000} // Amount of stars (default=5000)
+  factor={7} // Size factor (default=4)
+  saturation={0} // Saturation 0-1 (default=0)
+  fade // Faded dots (default=false)
+/>
+      <ambientLight intensity={1} />
+      <mesh ref={cloudyRef} position={[0,0,3]}>
+        <sphereGeometry args={[1.002, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsmap}
+          opacity={0.2}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
+      <mesh ref={earthRef} position={[0,0,3]}>
+        <sphereGeometry  args={[1.0, 32, 32]} />
+        <meshPhongMaterial specularMap={specularmap} />
+        <meshStandardMaterial map={daymap} normalmap={normalmap} />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={2.9}
+   
+        />
+      </mesh>
+      {/* <mesh>
+      <RoundedBox sphereGeometry
+  args={[2, 2, 2]} // Width, Height and Depth of the box
+  
+  radius={0.01} // Border-Radius of the box
+  smoothness={1} // Optional, number of subdivisions
+   // All THREE.Mesh props are valid
+>
+<meshPhongMaterial specularMap={specularmap} />
+        <meshStandardMaterial map={daymap} normalmap={normalmap} />
+  <meshPhongMaterial attach="material" color="#f3f3f3" wireframe />
+  <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={2.9}
+   
+        />
+</RoundedBox>
+</mesh> */}
+    </>
+  );
+};
+
+export default Dndex;
